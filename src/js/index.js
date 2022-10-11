f8180649e07e97da16cb43e119e6a27c4d38f179
damir <dimayu82@gmail.com>
2022-10-11 14:53:58 +0300

    Finish project

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,36 @@
-$(document).ready(function () {
-    //Menu button on click event
-    $('.mobile-nav-button').on('click', function () {
-        // Toggles a class on the menu button to transform the burger menu into a cross
-        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)").toggleClass("mobile-nav-button__line--1");
-        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)").toggleClass("mobile-nav-button__line--2");
-        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)").toggleClass("mobile-nav-button__line--3");
+let priceFull,
+    coming;
+let num = document.getElementById("num");
+let route = document.getElementById("route").value;
+let time = document.getElementById("time").value;
+let priceOne = 700;
+let priceTwo = 1200;
+let timeFull = 50;
 
-        // Toggles a class that slides the menu into view on the screen
-        $('.mobile-menu').toggleClass('mobile-menu--open');
-        return false;
-    });
-});
+num.addEventListener("input",
+    function (e) {
+        this.value = this.value.replace(/[^\d.]/g, "");
+    }
+);
+
+num.onblur = function () {
+    num = this.value;
+    priceFull = priceOne * num;
+};
+
+document.getElementById("btn").onclick = function () {
+    time = document.getElementById("time").value.substr(0, 5);
+    let hours = Number(time.substr(0, 2) * 60);
+    let minutes = Number(Number(time.substr(3, 5)) + timeFull + hours);
+    let minutes1 = minutes % 60;
+    let hours1 = (minutes - minutes1) / 60;
+    if (minutes1 < 10) minutes1 = "0" + minutes1;
+    if (hours1 < 10) hours1 = "0" + hours1;
+    coming = hours1 + ":" + minutes1;
+
+    document.getElementById("description").innerHTML =
+        "<p>\n" +
+        "Вы выбрали " + num + " билета по маршруту " + route + " стоимостью " + priceFull +
+        ". Это путешествие займет у вас " + timeFull + " минут. " +
+        "Теплоход отправляется в " + time + ", а прибудет в " + coming + "</p>";
+};
